2a6588ef0b231cb0703cc0144754f27a3e235a0b
vitor hugo <vhrocha17@gmail.com>
2022-01-18 20:48:51 -0300

    subindo a galeria de fotos mais os buttons e os arquivos jquery com seus plugins

---
+++ projeto-galeria/src/index.js
@@ -0,0 +1,13 @@
+//Arquivos sass
+
+import './scss/index.css';
+
+//Dependencias
+
+import 'jquery';
+import 'bootstrap';
+
+//arquivos js
+
+import './js/core/includes';
+import './js/plugins/cityButtons';
